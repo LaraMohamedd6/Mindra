36f9e7bbadd9671af2c971b027feaaf8e21d2ac4
KenzyEssam <120500567+KenzyEssam@users.noreply.github.com>
2025-06-12 23:43:07 +0300

    kanozaaa

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   }, 
-  base: process.env.VITE_BASE_PATH || "/zen-zone-connect",
+  base: process.env.VITE_BASE_PATH || "/",
 }));
